8cc2393cd7c9e0def9b9ecbdabbebf7da528260c
osama_mohamed98 <om107547@gmail.com>
2022-01-16 22:03:24 +0300

    finish app theme

--- lib/presentation/app_dimen.dart
+++ lib/presentation/app_dimen.dart
@@ -17,6 +17,7 @@ class AppPadding {
 }
 
 class AppSize {
+  static const double s1_5 = 1.5;
   static const double s4 = 4.0;
   static const double s8 = 8.0;
   static const double s12 = 12.0;
@@ -27,5 +28,4 @@ class AppSize {
   static const double s28 = 28.0;
   static const double s60 = 60.0;
   static const double s65 = 65.0;
-
 }
